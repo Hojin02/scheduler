df4a4f0890dc8c01ad739bc26a115f2778f884c8
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2024-12-09 14:55:35 +0900

    💡 Comment(schedule): add and update comments

--- src/main/java/com/example/scheduler/service/scheduleService/ScheduleServiceImpl.java
+++ src/main/java/com/example/scheduler/service/scheduleService/ScheduleServiceImpl.java
@@ -28,25 +28,25 @@ public class ScheduleServiceImpl implements ScheduleService {
         this.session = session;
     }
 
-    @Override
+    @Override               // 새 일정 추가
     public ScheduleResponseDto saveSchedule(ScheduleRequestDto dto) {
-        if (!loginCheck(session)) {
+        if (!loginCheck(session)) { // 로그인 한 사용자만 이용가능한 서비스. 로그인 체크
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This service requires login.");
         }
-        if (dto.getContents() == null) {
+        if (dto.getContents() == null) { // 필수 입력값(파라미터) : contents
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The author is required value.");
         }
-        Schedule schedule = new Schedule(
+        Schedule schedule = new Schedule( // 로그인한 유저의 아이디와, 입력값(contents) 를 넘기기 위해 객체 생성
                 (String) session.getAttribute("userId"),
                 dto.getContents()
         );
-        String authorName = (String) session.getAttribute("userName");
+        String authorName = (String) session.getAttribute("userName"); // 유저의 이름도 함께 Response하기 위해 세션에서 이름 가져옴.
         ScheduleResponseDto scheduleResponseDto = scheduleRepository.saveSchedule(schedule);
         scheduleResponseDto.setAuthor(authorName);
         return scheduleResponseDto;
     }
 
-    @Override
+    @Override                       // 날짜와 작성자 아이디로 필터링 하여 조회, 페이징 구현
     public List<ScheduleResponseDto> findSchedulesByFilters(String authorId, String date, int page, int size) {
         List<ScheduleResponseDto> result = scheduleRepository.findSchedulesByFilters(authorId, date);
         if (result.isEmpty()) {
@@ -59,28 +59,30 @@ public class ScheduleServiceImpl implements ScheduleService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The range you entered is greater than the range of the data.");
         }
         int end = Math.min(start + size, total);
+        // 전체 스케줄 중 페이징하여 잘라냄.
         List<ScheduleResponseDto> pagingList = result.subList(start,end);
         return pagingList;
     }
 
-    @Override
+    @Override               // 스케줄의 고유 id로 스케줄 조회
     public ScheduleResponseDto findScheduleById(Long id) {
         return scheduleRepository.findScheduleByIdOrElseThrow(id);
     }
 
-    @Override
+    @Override // 스케줄의 고유id로 스케줄 삭제
     public int deleteSchedule(Long id, UserRequestDto dto) {
         if (!loginCheck(session)) {// 로그인 한 상태여야함.
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This service requires login.");
         }
         dto.setId((String) session.getAttribute("userId"));
         // 로그인한 사람과 작성글의 아이디가 같아야함.
+        // 본인의 게시글만 삭제가능
         String scheduleAuthorId = scheduleRepository.findScheduleByIdOrElseThrow(id).getAuthorId();
         String loginedUserId = dto.getId();
         if (!scheduleAuthorId.equals(loginedUserId)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You can only delete your posts");
         }
-        // 로그인한 사람의 비밀번호와 입력한 비밀번호가 같아야함
+        // 로그인한 사람의 비밀번호와 입력한 비밀번호가 같아야함(비밀번호 이중체크)
         UserLoignRequestDto userLoignRequestDto = new UserLoignRequestDto(loginedUserId, dto.getPassword());
         if (!userRepository.login(userLoignRequestDto)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid input. Please check again.");
@@ -109,11 +111,12 @@ public class ScheduleServiceImpl implements ScheduleService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The input value is invalid and cannot be " +
                     "modified.");
         }
+        // 수정 후 해당 일정을 가져와 리턴.
         ScheduleResponseDto responseDto = scheduleRepository.findScheduleByIdOrElseThrow(id);
         return responseDto;
     }
 
-    @Override
+    @Override // 로그인 된 사용자인지 확인하는 함수.
     public boolean loginCheck(HttpSession session) {
         if (session.getAttribute("userId") != null) {
             return true;
